eff9936db890a2e6a8536f4dc2fb59846f4a14c4
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2025-01-12 23:37:56 +0530

    tailwind added

---
+++ client/src/pages/header.tsx
@@ -0,0 +1,16 @@
+
+
+export const Header = () => {
+   
+    return <div className="p-5 bg-slate-100 shadow-md">
+           <div className="flex justify-between items-center">
+              <div className="text-2xl font-bold">
+                BlogPost
+              </div>
+              <div className="flex justify-between gap-4">
+                <div>Create</div>
+                <div>User</div>
+              </div>
+           </div>
+    </div>
+}
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,18 @@
+import { Header } from "./header";
+
 
 export default function home() {
     return(
         <div>
-            <h1>home pages</h1>
+            <div>
+                 <Header />
+            </div>
+            <div>
+                main
+            </div>
+            <div>
+                footer
+            </div>
         </div>
     )
 }
